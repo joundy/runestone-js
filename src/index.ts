fc98a63aaec4216f74469d1491877dc18cd3b298
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2024-04-16 03:46:44 +0700

    fix: import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./Flag";
+export * from "./Rune";
 export * from "./RuneId";
 export * from "./Runestone";
+export * from "./SpacedRune";
 export * from "./Tag";
 export * from "./types";
